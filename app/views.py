7e502ac05ea20adc7adc1ebe1fcdb2f9c1ad262b
Larry Hernandez <larryhernanjr@gmail.com>
2017-06-09 16:12:29 -0400

    add register logic and template

--- app/views.py
+++ app/views.py
@@ -1,2 +1,23 @@
 from .models import User
 from flask import
+
+app = flask(__name__)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if len(username) < 1:
+            flash('Your username must be at least one character.')
+        elif len(password) < 5:
+            flash('Your password must be longer than 5 characters.')
+        elif not User(username).register(password):
+            flash('Username already exists!')
+        else:
+            session['username'] = username
+            flash['signed in']
+            return redirect(url_for('index'))
+
+    return render_template('register.html')
